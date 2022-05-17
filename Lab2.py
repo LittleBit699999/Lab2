bb8291ac62f39c73085d78d7dbeebac3c9b14a23
Lucas <lucaswai.21@ichat.sp.edu.sg>
2022-05-18 06:18:23 +0800

    updated

--- Lab2.py
+++ Lab2.py
@@ -1,3 +1,6 @@
+import statistics
+
+
 def display_main_menu():
     print("display_main_menu")
     print("Enter some numbers separated by comas(e.g. 5,67,32)")
@@ -5,8 +8,8 @@ def display_main_menu():
 
 def get_user_input():
     num_list = []
-    strinput = input()
-    str_list = strinput.split(",")
+    str_input = input()
+    str_list = str_input.split(",")
     for item in str_list:
         num_list.append(int(item))
 
@@ -16,7 +19,7 @@ def get_user_input():
 def calc_average_temperature(num_list):
     print("calc_average_temperature")
     avg = sum(num_list)/len(num_list)
-    return round(avg,2)
+    return round(avg, 2)
 
 
 def calc_min_max_temperature(num_list):
@@ -27,6 +30,17 @@ def calc_min_max_temperature(num_list):
     return temp_list
 
 
+def sort_temperature(num_list):
+    print("sort_temperature")
+    num_list.sort()
+    return num_list
+
+
+def calc_median_temperature(num_list):
+    print("calc_median_temperature")
+    return statistics.median(num_list)
+
+
 def main():
     print("ET0735 (DevOps for AloT) - lab2 - Introduction to Python")
     display_main_menu()
@@ -34,9 +48,13 @@ def main():
     print(num_list)
     print(str(type(num_list[0])))
     average = calc_average_temperature(num_list)
-    print("Average temperature is", average)
+    print("Average temperature is", average, "degree celsius")
     temp_list = calc_min_max_temperature(num_list)
-    print("The minimum and maximum temperature in the num_list are", temp_list, "respectively")
+    print("The minimum and maximum temperature in the num_list are", temp_list, "degree celsius respectively")
+    sort_temp = sort_temperature(num_list)
+    print("Temperatures in ascending order", sort_temp)
+    median_temp = calc_median_temperature(num_list)
+    print("The median temperature is", median_temp, "degree celsius")
 
 
 if __name__ == "__main__":
--- Test_Lab2.py
+++ Test_Lab2.py
@@ -2,7 +2,6 @@ import Lab2
 
 
 def test_lab2_avg():
-    num_list = []
     test_list = [5, 67, 32]
     avg = Lab2.calc_average_temperature(test_list)
 
@@ -10,8 +9,20 @@ def test_lab2_avg():
 
 
 def test_lab2_minmax():
-    num_list = []
     test_list = [5, 67, 32]
     temp_list = Lab2.calc_min_max_temperature(test_list)
 
-    assert(temp_list == [5, 67])
+    assert (temp_list == [5, 67])
+
+
+def test_lab2_sort_temp():
+    test_list = [5, 67, 32]
+    sort_list = Lab2.sort_temperature(test_list)
+    assert (sort_list == [5, 32, 67])
+
+
+def test_lab2_calc_median():
+    test_list = [5, 67, 32]
+    cal_m = Lab2.calc_median_temperature(test_list)
+    assert (cal_m == 32)
+
